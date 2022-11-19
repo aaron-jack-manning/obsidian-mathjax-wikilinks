a1ad818a91c01985b34eb41db4ce2c492df6ff2b
aaron-jack-manning <contact@aaronmanning.net>
2022-11-19 17:13:42 +1100

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Obsidian MathJax WikiLinks
+
+Obsidian plugin to render MathJax within WikiLinks.
+
+## Example
+
+The WikiLink `[[sigma-algebra|$\sigma$-algebra]]` renders in Obsidian without rendering the MathJax expression from the alias, whereas `[$\sigma$-algebra](sigma-algebra.md)` will render correctly. This plugin unifies the two versions, rendering MathJax properly in both cases.
+
+## Installation
+
+To install this plugin simply copy `main.js` and `manifest.json` into a folder `VAULT_PATH/.obsidian/plugins/obsidian-mathjax-wikilinks`.
---
+++ main.ts
@@ -0,0 +1,43 @@
+import { Plugin, loadMathJax, finishRenderMath, renderMath } from 'obsidian';
+
+
+export default class MyPlugin extends Plugin {
+
+	async onload() {
+
+        await loadMathJax();
+
+        this.registerMarkdownPostProcessor((element, context) => {
+
+            element.querySelectorAll('.internal-link').forEach((link) => {
+				let linkText = link.textContent.trim();
+
+				if (linkText.contains("$")) {
+					
+					let isMath = false;
+					let sections = linkText.split("$");
+
+					link.innerText = '';
+					for (let i = 0; i < sections.length; i++) {
+						let text = sections[i];
+
+						if (text != "") {
+							if (isMath) {
+								let mathLink = link.createSpan();
+								mathLink.replaceWith(renderMath(text, false));
+							} else {
+								let wordLink = link.createSpan();
+								wordLink.innerText += text;
+							}
+						}
+
+						isMath = !isMath;
+					}
+
+					finishRenderMath();
+                }
+            })
+        });
+	}
+}
+
